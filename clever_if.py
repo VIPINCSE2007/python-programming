7d62b2f9d9c2d10916496aff54ad3fa5dc033d34
VIPINCSE2007 <vipinsinghb2007@gmail.com>
2026-06-18 15:13:17 +0530

    ternary operator clever if

--- clever_if.py
+++ clever_if.py
@@ -1,3 +1,7 @@
 age = int(input("enter age : "))
 vote = ("not elligible ","yes you can vote") [age>=18]
 print(vote)
+
+sal = int(input("enter your salery : "))
+tax = sal*(0.1,0.2) [sal>50000]
+print("your salary tax is :" , tax)
