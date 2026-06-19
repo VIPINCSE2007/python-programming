c4745107ffb2199145df2af5a3a209a2d1500f17
VIPINCSE2007 <vipinsinghb2007@gmail.com>
2026-06-19 15:13:18 +0530

    str.count()

---
+++ str.count.py
@@ -0,0 +1,8 @@
+# str.count() this string function count how many times a word or a letter has been occoured in a string
+
+str = "i am learning python from apna college youtube channel"
+print(str.count("o"))   # output is 4
+
+print(str.count("a"))   #output is 5
+
+
