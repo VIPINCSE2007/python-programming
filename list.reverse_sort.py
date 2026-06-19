27232aa57d2aaf46cfd37f6377e05919dc7a1d91
VIPINCSE2007 <vipinsinghb2007@gmail.com>
2026-06-19 16:58:28 +0530

    list functions

---
+++ list.reverse_sort.py
@@ -0,0 +1,6 @@
+# list can be reverse sort by 
+# list.sort(reverse=True)
+
+list =[1,2,3,4,5,6]
+list.sort(reverse=True)
+print(list)
---
+++ list.sort.py
@@ -0,0 +1,8 @@
+# list.sort() this list meathod make the order in ascending
+list = [1,2,3,4,5]
+print(list.sort())    #this returns none as it directly changes the orignal list
+print(list)           #prints the new modified list
+
+# can be done directly without printing none 
+list.sort()
+print(list)
