4e568654d0eb212f9f1913ae1a758550f2f4fc0e
VIPINCSE2007 <vipinsinghb2007@gmail.com>
2026-06-19 15:35:46 +0530

    conditional statements

---
+++ conditional.statement.py
@@ -0,0 +1,10 @@
+marks = int(input("enter your marks : "))
+
+if(marks>=90):
+    print("Grade = A")
+elif(marks<90 and marks>=80):
+    print("Grade = B")
+elif(marks<80 and marks>=70):
+    print("Grade = C")
+else:
+    print("Grade = D")
