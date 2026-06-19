f69501565c47ed8bcb2acef82b5edeb028e55d06
VIPINCSE2007 <vipinsinghb2007@gmail.com>
2026-06-19 14:42:32 +0530

    str.capitalize()

---
+++ str.capitalize.py
@@ -0,0 +1,10 @@
+#str.capitalize() this string function capitalise the first letter of the string 
+#it doesnt change the orignal string if you want to change the orignal string you have to store it in another string
+
+str = "i am vipin"
+print(str.capitalize())
+print(str)
+
+#store it in another variable , to change the orignal string 
+str = str.capitalize()
+print(str)
