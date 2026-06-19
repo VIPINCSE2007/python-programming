1aa010089e35df3e1a0784ea9be0e746c8877ecf
VIPINCSE2007 <vipinsinghb2007@gmail.com>
2026-06-19 14:46:46 +0530

    str.replace()

---
+++ str.replace.py
@@ -0,0 +1,6 @@
+# str.replace can replace any word or letter from the string
+#str.replace("old","new")
+
+str = "I am learning python from youtube"
+print(str.replace("o","a"))
+print(str.replace("python","java"))
