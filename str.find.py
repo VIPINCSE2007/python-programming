5839e0c7cb116052b991a832ae4f459f57810f6c
VIPINCSE2007 <vipinsinghb2007@gmail.com>
2026-06-19 14:57:47 +0530

    str.find()

---
+++ str.find.py
@@ -0,0 +1,4 @@
+# str.find() this string function tells the address of the first letter of the word written in the function 
+#index 012345
+str = "i am learning python from youtube"
+print(str.find("learning"))
