5881d474593e26d58ec297d641eda49a8012cddf
VIPINCSE2007 <vipinsinghb2007@gmail.com>
2026-06-22 10:31:18 +0530

    tuples functions()

---
+++ tuple.count.py
@@ -0,0 +1,5 @@
+#in this function , it will print how many times the element is written in the tupple
+#tuple.count(element)
+
+tuple = (2,1,4,3,2,4,3,2,5,6,3,7,3,3)
+print(tuple.count(3))
---
+++ tuple.index.py
@@ -0,0 +1,4 @@
+#in the parenthesis we will write the element and the index function will print the index of the element
+#tuple.index(element)
+tuple = (3,1,2,5)
+print(tuple.index(5))
