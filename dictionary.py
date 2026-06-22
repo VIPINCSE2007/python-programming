2f589037f154a1b7c9609ddb8c79cc29cb3ba1fa
VIPINCSE2007 <vipinsinghb2007@gmail.com>
2026-06-22 11:34:13 +0530

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+# in a dictionary we store data in akey value pair  "key": value
+
+dict = {
+    "name" : "vipin",
+    "age" : 19,
+    "marks" : [90,92,94,85],
+    21 : 19,               #key can be integer
+     (21,25) : "marks"     #key can be tuple
+}
+print(dict)
