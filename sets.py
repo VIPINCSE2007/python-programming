dded91caff18fbab748e149d204809254db42246
VIPINCSE2007 <vipinsinghb2007@gmail.com>
2026-06-23 10:11:13 +0530

    sets

---
+++ sets.py
@@ -0,0 +1,7 @@
+# set is collection of unordered items
+# each element in sets is unique and unmutable (eg int ,float , boolean string tuple ) (list and dictionary cant be stored in sets as they are mutable)
+
+nums = {1,2,3,4,5,6}
+set = {1,2,2,2}           # this will be seen as set = {1,2}
+
+#repeated elements are stored only once
